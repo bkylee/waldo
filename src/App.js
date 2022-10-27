44ea56fa9719a11db4440ba516bff513583fc16f
bkylee <brian.ky.lee@outlook.com>
2022-10-27 18:37:07 -0400

    Started on project by creating react app and selecting first photo to start mapping

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Welcome from './components/Welcome';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/waldo' element={<Welcome />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/components/Battle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import battle from '../images/battle.png'
+
+const Battle = () => {
+  return (
+    <div>Battle</div>
+  )
+}
+
+export default Battle
