a179db2524fd7e6da3ffcf2f88df54146d39db81
bkylee <brian.ky.lee@outlook.com>
2022-11-02 20:05:19 -0400

    gp-pages set up and set height for page

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 import battle from '../images/battle/battle.png'
 const Page = () => {
   return (
-  <>
+  <div id='homepage'>
   <h1 id='Title'>Where's Waldo?</h1>
   <nav>
     <h2 id='select'>Select an image to look for Waldo!</h2>
     <Link to='/battle' className='selector'><img src={battle} height='auto' width='300px'/>Battle</Link>
   </nav>
-  </>
+  </div>
   )
 }
 
