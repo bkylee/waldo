169271263985aafbea882823001448a4e3344570
bkylee <brian.ky.lee@outlook.com>
2022-11-06 15:47:52 -0500

    able to fetch scores

---
+++ src/components/ShowScore.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowScore = ({name, score}) => {
+  return (
+    <div>{name} : {score}</div>
+  )
+}
+
+export default ShowScore
--- src/components/Win.jsx
+++ src/components/Win.jsx
@@ -1,22 +1,28 @@
-import { doc, getDoc, orderBy, query, collection} from "firebase/firestore";
-import React, { useState, useEffect } from 'react'
+import { doc, getDocs, orderBy, query, collection, setDoc, limit} from "firebase/firestore";
+import React, { useState } from 'react'
 import {db} from '../App'
-
+import uniqid from 'uniqid'
 const Win = ({timer, PA }) => {
     const [userInfo, setUserInfo] = useState("");
     const [scores, setScores] = useState([]);
-    const scoreRef = doc(db, "Battle", "High Score");
+    const scoreRef = collection(db,"Scores");
+    const q = query(scoreRef, orderBy("score"), limit(5))
     
     const fetchscore = async () =>{
-        const querySnap = await getDoc(scoreRef)
-        console.log(querySnap.data())
+        const querySnap = await getDocs(q);
+        setScores(querySnap.docs.map(scores=>({...scores.data()})));
     }    
     fetchscore();
-    // useEffect(()=>{
-        //     setScores(getDoc(q));
-        //     console.log(scores)
-        // },[scores]);
+    
+    const onClick = async () =>{
+        await setDoc(doc(db, "Scores", `${userInfo}`), {
+            name: userInfo,
+            score: timer
+        });
+        fetchscore();
+    };
 
+    
   return (
     <div>
         <h1>You found everyone!</h1>
@@ -25,12 +31,18 @@ const Win = ({timer, PA }) => {
             <form>
                 <label htmlFor='name' >Name
                 <input type='text' id='name' value={userInfo} onChange={(e)=>{setUserInfo(e.target.value)}}/> </label>
-                <button type='button' >Submit</button>
+                <button type='button' onClick={onClick} >Submit</button>
             </form>
         </div>
         <div id='scores'>    
             <h1>High Scores</h1>
-            {/* {scores.map(score=>(<div>{score}</div>))} */}
+            {
+                scores.map(score=>{
+                    return(
+                        <div key={uniqid()}>{score.name}:{score.score}</div>
+                    )
+                })
+            }
         </div>
         <button type='button' onClick={PA}>Play Again</button>
         
